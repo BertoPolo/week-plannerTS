d1c7e4584c4d6e69fbb2a611579b6a4f0d626010
BertoPolo <bertopolo91@gmail.com>
2023-10-13 19:31:48 +0200

    Update api.ts

--- api.ts
+++ api.ts
@@ -1,10 +1,10 @@
 import { ITask } from "./types"
 
-const baseURL = "http://localhost:3001"
+const baseURL = "http://localhost:3001/"
 
 export const getAllTasks = async (): Promise<ITask[]> => {
-  const res = await fetch(`${baseURL}/tasks`)
-  const tasks = res.json()
-  console.log(await tasks)
+  const res = await fetch(`${baseURL}tasks`)
+  const tasks = await res.json()
+  console.log(tasks)
   return tasks
 }
