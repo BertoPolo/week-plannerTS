6263424d2f651b8a3fd10b88016d4cacba32ffb5
Alberto Polo <bertopolo91@gmail.com>
2023-10-16 10:57:07 +0200

    inputs setup

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,11 +1,18 @@
 "use client"
-import { createTask } from "@/functions/functionsAndVars"
-import { Textarea } from "@nextui-org/react"
-import React, { useState } from "react"
+// import { Textarea } from "@nextui-org/react"
+import React, { FormEventHandler, useState } from "react"
 // import { inputAndLabel } from "./InputAndLabel"
 
 const Modal = () => {
-  const [isModal, setIsModal] = useState<boolean>(false)
+  const [newTaskNameValue, setNewTaskNameValue] = useState<string>("")
+  const [newDateValue, setNewDateValue] = useState<string>("")
+  const [newStartTimeValue, setNewStartTimeValue] = useState<string>("")
+  const [newEndTimeValue, setNewEndTimeValue] = useState<string>("")
+  const [newDescriptionValue, setNewDescriptionValue] = useState<string>("")
+
+  const createTask: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault()
+  }
 
   return (
     <div>
@@ -14,20 +21,39 @@ const Modal = () => {
         Add task
       </label>
 
-      {/* Put this part before </body> tag */}
       <input type="checkbox" id="AddTaskModal" className="modal-toggle" />
       <div className="modal">
         <div className="modal-box">
-          <h3 className="text-lg font-bold">Hello!</h3>
-          <p className="py-4">This modal works with a hidden checkbox!</p>
+          <h3 className="text-lg font-bold">Add your task!</h3>
 
-          <form action="">
-            <input type="text" title="Task title" placeholder="enter task here" />
-            <input type="date" title="Date" />
-            <input type="time" title="Start Time" />
-            <input type="time" title="End Time" />
-            <Textarea type="textarea" title="Description" minRows={3} placeholder="Insert description" />
-            <button type="submit">Submit</button>
+          <form onSubmit={createTask}>
+            <input
+              type="text"
+              value={newTaskNameValue}
+              onChange={(e) => setNewTaskNameValue(e.target.value)}
+              title="Task title"
+              placeholder="enter task here"
+              className="block"
+            />
+            <input type="date" value={newDateValue} onChange={(e) => setNewDateValue(e.target.value)} title="Date" className="block" />
+            <input
+              type="time"
+              value={newStartTimeValue}
+              onChange={(e) => setNewStartTimeValue(e.target.value)}
+              title="Start Time"
+              className="block"
+            />
+            <input type="time" value={newEndTimeValue} onChange={(e) => setNewEndTimeValue(e.target.value)} title="End Time" className="block" />
+            <textarea
+              className="textarea textarea-info"
+              value={newDescriptionValue}
+              onChange={(e) => setNewDescriptionValue(e.target.value)}
+              title="Description"
+              placeholder="Insert description"
+            />
+            <button type="submit" className="btn block">
+              Submit
+            </button>
           </form>
         </div>
         <label className="modal-backdrop" htmlFor="AddTaskModal">
