7948a0a75801981e66a84284d66708e86939bb3a
Alberto Polo <bertopolo91@gmail.com>
2023-10-20 18:46:54 +0200

    prepare delete function with confirmation.

--- components/Task.tsx
+++ components/Task.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { FormEventHandler, useState } from "react"
-import { modifyTask } from "@/api"
+import { deleteTask, modifyTask } from "@/api"
 import { useRouter } from "next/navigation"
 import { TaskProps, ITask } from "@/types"
 import { FiEdit } from "react-icons/fi"
@@ -56,6 +56,24 @@ const Task = ({ tasks, weekDay }: TaskProps) => {
     router.refresh()
   }
 
+  const handleDeleteTaskForm: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault()
+
+    // await deleteTask({
+    //   id: selectedId,
+    //   taskName: selectedTaskName,
+    //   startTime: selectedStartTime,
+    //   endTime: selectedEndTime,
+    //   // date: newDateValue,
+    //   isDone: false,
+    //   description: selectedDescription,
+    //   dayOfWeek: selectedDayOfWeek,
+    // })
+    resetStates()
+    //refreshing
+    router.refresh()
+  }
+
   return (
     <>
       {/* display every day with it's tasks */}
@@ -78,18 +96,17 @@ const Task = ({ tasks, weekDay }: TaskProps) => {
                     {/* icons EDIT and DELETE */}
                     <span className="flex">
                       <label htmlFor="EditTaskModal">
-                        <FiEdit
-                          size={15}
-                          className="cursor-pointer text-blue-400 mx-3"
-                          onClick={() => {
-                            changeStates(task)
-                            console.log("test", selectedTaskName)
-                          }}
-                        />
+                        <FiEdit size={15} className="cursor-pointer text-blue-400 mx-3" onClick={() => changeStates(task)} />
                       </label>
 
                       <label htmlFor="DeleteTaskModal">
-                        <ImBin size={15} className="cursor-pointer text-red-600" />
+                        <ImBin
+                          size={15}
+                          className="cursor-pointer text-red-600"
+                          onClick={(e: any) => {
+                            confirm("Are you sure to delete this") ? handleDeleteTaskForm(e) : console.log("Task not deleted")
+                          }}
+                        />
                       </label>
                     </span>
                   </p>
@@ -108,90 +125,88 @@ const Task = ({ tasks, weekDay }: TaskProps) => {
         <div className="modal">
           <div className="modal-box">
             <h3 className="text-lg font-bold mb-4">Let`s modify your task</h3>
-            {/* <form onSubmit={handleEditTaskForm}> */}
-            <input
-              type="text"
-              value={selectedTaskName}
-              onChange={(e) => setSelectedTaskName(e.target.value)}
-              title="Task title"
-              placeholder="--> enter task title here"
-              className="block"
-              required
-            />
-            {/* <input type="date" value={newDateValue} onChange={(e) => setNewDateValue(e.target.value)} title="Date" className="block" /> */}
+            <form onSubmit={handleEditTaskForm}>
+              <input
+                type="text"
+                value={selectedTaskName}
+                onChange={(e) => setSelectedTaskName(e.target.value)}
+                title="Task title"
+                placeholder="--> enter task title here"
+                className="block"
+                required
+              />
+              {/* <input type="date" value={newDateValue} onChange={(e) => setNewDateValue(e.target.value)} title="Date" className="block" /> */}
 
-            {/* weekday dropdown */}
-            <div className="dropdown dropdown-right">
-              <label tabIndex={0} className="btn m-1">
-                {selectedDayOfWeek}
-              </label>
+              {/* weekday dropdown */}
+              <div className="dropdown dropdown-right">
+                <label tabIndex={0} className="btn m-1">
+                  {selectedDayOfWeek}
+                </label>
 
-              <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                <li>
-                  <p onClick={() => setSelectedDayOfWeek("Monday")}>Monday</p>
-                </li>
+                <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                  <li>
+                    <p onClick={() => setSelectedDayOfWeek("Monday")}>Monday</p>
+                  </li>
 
-                <li>
-                  <p onClick={() => setSelectedDayOfWeek("Tuesday")}>Tuesday</p>
-                </li>
+                  <li>
+                    <p onClick={() => setSelectedDayOfWeek("Tuesday")}>Tuesday</p>
+                  </li>
 
-                <li>
-                  <p onClick={() => setSelectedDayOfWeek("Wednesday")}>Wednesday</p>
-                </li>
+                  <li>
+                    <p onClick={() => setSelectedDayOfWeek("Wednesday")}>Wednesday</p>
+                  </li>
 
-                <li>
-                  <p onClick={() => setSelectedDayOfWeek("Thursday")}>Thursday</p>
-                </li>
+                  <li>
+                    <p onClick={() => setSelectedDayOfWeek("Thursday")}>Thursday</p>
+                  </li>
 
-                <li>
-                  <p onClick={() => setSelectedDayOfWeek("Friday")}>Friday</p>
-                </li>
+                  <li>
+                    <p onClick={() => setSelectedDayOfWeek("Friday")}>Friday</p>
+                  </li>
 
-                <li>
-                  <p onClick={() => setSelectedDayOfWeek("Saturday")}>Saturday</p>
-                </li>
+                  <li>
+                    <p onClick={() => setSelectedDayOfWeek("Saturday")}>Saturday</p>
+                  </li>
 
-                <li>
-                  <p onClick={() => setSelectedDayOfWeek("Sunday")}>Sunday</p>
-                </li>
-              </ul>
-            </div>
-            {/* Start time */}
-            <label htmlFor="" className="block">
-              Start time
-            </label>
-            <input
-              type="time"
-              value={selectedStartTime}
-              onChange={(e) => setSelectedStartTime(e.target.value)}
-              title="Start Time"
-              className="block"
-              required
-            />
-            {/* End time */}
-            <label htmlFor="">End time</label>
-            <input
-              type="time"
-              value={selectedEndTime}
-              onChange={(e) => setSelectedEndTime(e.target.value)}
-              title="End Time"
-              className="block"
-              required
-            />
-            {/* Description */}
-            <textarea
-              className="textarea textarea-info"
-              value={selectedDescription}
-              onChange={(e) => setSelectedDescription(e.target.value)}
-              title="Description"
-              placeholder="Insert description"
-              required
-            />
+                  <li>
+                    <p onClick={() => setSelectedDayOfWeek("Sunday")}>Sunday</p>
+                  </li>
+                </ul>
+              </div>
+              {/* Start time */}
+              <span className="block">Start time</span>
+              <input
+                type="time"
+                value={selectedStartTime}
+                onChange={(e) => setSelectedStartTime(e.target.value)}
+                title="Start Time"
+                className="block"
+                required
+              />
+              {/* End time */}
+              <span>End time</span>
+              <input
+                type="time"
+                value={selectedEndTime}
+                onChange={(e) => setSelectedEndTime(e.target.value)}
+                title="End Time"
+                className="block"
+                required
+              />
+              {/* Description */}
+              <textarea
+                className="textarea textarea-info"
+                value={selectedDescription}
+                onChange={(e) => setSelectedDescription(e.target.value)}
+                title="Description"
+                placeholder="Insert description"
+                required
+              />
 
-            <button type="submit" className="btn block">
-              Submit
-            </button>
-            {/* </form> */}
+              <button type="submit" className="btn block">
+                Submit
+              </button>
+            </form>
           </div>
           <label className="modal-backdrop" htmlFor="EditTaskModal">
             Close
