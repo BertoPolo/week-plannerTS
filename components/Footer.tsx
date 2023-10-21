74a767aa77720bddc6b3f256742998bbf30ece90
Alberto Polo <bertopolo91@gmail.com>
2023-10-21 19:30:58 +0200

    fixing footer to the bottom

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,11 +2,13 @@ import React from "react"
 
 const Footer = () => {
   return (
-    <div className="mt-5 relative h-20 w-screen bg-slate-200">
-      <div className="absolute inset-x-0 bottom-0 h-16 grid grid-cols-3 text-center">
-        <h4>About</h4>
-        <h4>Used Tecnologies</h4>
-        <h4>Links</h4>
+    <div className="static">
+      <div className="absolute bottom-0 left-0 mt-5 h-20 w-screen bg-slate-200">
+        <div className="h-16 grid grid-cols-3 text-center pt-5">
+          <h4>About</h4>
+          <h4>Used Tecnologies</h4>
+          <h4>Links</h4>
+        </div>
       </div>
     </div>
   )
