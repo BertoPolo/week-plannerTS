20086db347690fbad79fe14baf4b1ecab358105f
Alberto Polo <bertopolo91@gmail.com>
2023-10-22 10:44:38 +0200

    fix footer scrolling. changing theme button's size, removing useclient, seems its not needed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import { BiLogoLinkedinSquare } from "react-icons/bi"
 
 const Footer = () => {
   return (
-    <div className="mt-5 w-screen bg-slate-200">
+    <div className="mt-5 bg-slate-200">
       <div className="grid grid-cols-3 text-center py-4">
         <div>
           <h4>
