f11c1059fa05bfe9d1baa43ce97c84d6cbe677d7
Alberto Polo <bertopolo91@gmail.com>
2023-10-22 10:38:02 +0200

    adding content to footer,cleaning and styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,39 @@
+import Link from "next/link"
 import React from "react"
+import { BiLogoLinkedinSquare } from "react-icons/bi"
 
 const Footer = () => {
   return (
-    <div className="">
-      <div className="left-0 mt-5 h-20 w-screen bg-slate-200">
-        <div className="h-16 grid grid-cols-3 text-center pt-5">
+    <div className="mt-5 w-screen bg-slate-200">
+      <div className="grid grid-cols-3 text-center py-4">
+        <div>
           <h4>
             <u>About</u>
           </h4>
+        </div>
+
+        <div>
           <h4>
             <u>Used Tecnologies</u>
           </h4>
+          <p>TypeScript</p>
+          <p>NextJs</p>
+          <p>TailWind CSS</p>
+          <p>DaisyUI</p>
+          <p>json-server</p>
+          <p></p>
+        </div>
+
+        <div>
           <h4>
             <u>Links</u>
           </h4>
+          <p>
+            <Link target="__blank" rel="noreferrer noopener" href="https://www.linkedin.com" className="cursor-pointer">
+              {/* <BiLogoLinkedinSquare /> */}
+              LinkedIn
+            </Link>
+          </p>
         </div>
       </div>
     </div>
