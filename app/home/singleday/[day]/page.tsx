af5ac1e86e3fbd9de88e1dab9392465659258183
Alberto Polo <bertopolo91@gmail.com>
2023-10-25 10:51:30 +0200

    handle if there's no tasks,

--- app/home/singleday/[day]/page.tsx
+++ app/home/singleday/[day]/page.tsx
@@ -5,42 +5,55 @@ import { getTasks } from "@/api"
 const SingleDay = async ({ params }: Params) => {
   const tasks = await getTasks(params.day)
 
-  tasks.sort((a, b) => {
-    if (a.startTime < b.startTime) {
-      return -1
-    }
-    if (a.startTime > b.startTime) {
-      return 1
-    }
-    return 0
-  })
-
+  //   sorting by startTime
+  if (tasks.length > 0) {
+    tasks.sort((a, b) => {
+      if (a.startTime < b.startTime) {
+        return -1
+      }
+      if (a.startTime > b.startTime) {
+        return 1
+      }
+      return 0
+    })
+  }
   return (
-    <div className="overflow-x-auto">
-      <table className="table table-sm">
-        {/* head */}
-        <thead>
-          <tr>
-            <th className="w-9">Start Time</th>
-            <th className="">Task Title</th>
-            <th>Description</th>
-            <th className="w-9">End Time</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tasks.map((task) => {
-            return (
-              <tr className="hover" key={task.id}>
-                <td>{task.startTime}</td>
-                <td>{task.taskName}</td>
-                <td>{task.description}</td>
-                <td>{task.endTime}</td>
+    <>
+      <div className="overflow-x-auto">
+        <table className="table table-sm">
+          {/* head */}
+          <thead>
+            <tr>
+              <th className="w-9">Start Time</th>
+              <th className="">Task Title</th>
+              <th>Description</th>
+              <th className="w-9">End Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tasks.length <= 0 ? (
+              <tr className="hover">
+                <td></td>
+                <td></td>
+                <td>There´s no plans for today!</td>
+                <td></td>
               </tr>
-            )
-          })}
-        </tbody>
-      </table>
-    </div>
+            ) : (
+              tasks.map((task) => {
+                return (
+                  <tr className="hover" key={task.id}>
+                    <td>{task.startTime}</td>
+                    <td>{task.taskName}</td>
+                    <td>{task.description}</td>
+                    <td>{task.endTime}</td>
+                  </tr>
+                )
+              })
+            )}
+          </tbody>
+        </table>
+      </div>
+    </>
   )
 }
 
