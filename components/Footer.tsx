2e600b1dbaa4927dd123115858be61f17ec4c941
Alberto Polo <bertopolo91@gmail.com>
2023-10-25 15:46:24 +0200

    fix footer at bottom, improvable but stay like this by now

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,50 +6,52 @@ import { FaConnectdevelop } from "react-icons/fa"
 
 const Footer = () => {
   return (
-    <footer className="footer p-10 mt-10 bg-base-200 text-base-content">
-      <aside>
-        <span className="text-7xl rotate-12">
-          <FaConnectdevelop />
-        </span>
-        <p>
-          Alberto Polo
-          <br />
-          NextJs Developer
-        </p>
-      </aside>
+    <div className="relative h-screen">
+      <footer className="absolute inset-x-0 bottom-0 footer p-10 mt-10 bg-base-200 text-base-content">
+        <aside>
+          <span className="text-7xl rotate-12">
+            <FaConnectdevelop />
+          </span>
+          <p>
+            Alberto Polo
+            <br />
+            NextJs Developer
+          </p>
+        </aside>
 
-      <nav>
-        <header className="footer-title">Stack</header>
-        <a className="link link-hover" target="_blank" rel="noopener noreferrer" href="https://www.typescriptlang.org/docs/">
-          TypeScript
-        </a>
-        <a className="link link-hover" target="_blank" rel="noopener noreferrer" href="https://nextjs.org/docs">
-          NextJs
-        </a>
-        <a className="link link-hover" target="_blank" rel="noopener noreferrer" href="https://tailwindcss.com/docs/installation">
-          TailWind CSS
-        </a>
-        <a className="link link-hover" target="_blank" rel="noopener noreferrer" href="https://daisyui.com/">
-          DaisyUI
-        </a>
-        <a className="link link-hover" target="_blank" rel="noopener noreferrer" href="https://www.npmjs.com/package/json-server">
-          json-server
-        </a>
-      </nav>
-      <nav>
-        <header className="footer-title">Social</header>
-        <div className="flex ">
-          <Link className="link link-hover" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/bertopolo">
-            <span className="text-4xl">
-              <BiLogoLinkedinSquare />
-            </span>
-          </Link>
-          <Link className="link link-hover mx-2" target="_blank" rel="noopener noreferrer" href="https://github.com/BertoPolo">
-            <span className="text-4xl">{<AiFillGithub />}</span>
-          </Link>
-        </div>
-      </nav>
-    </footer>
+        <nav>
+          <header className="footer-title">Stack</header>
+          <a className="link link-hover" target="_blank" rel="noopener noreferrer" href="https://www.typescriptlang.org/docs/">
+            TypeScript
+          </a>
+          <a className="link link-hover" target="_blank" rel="noopener noreferrer" href="https://nextjs.org/docs">
+            NextJs
+          </a>
+          <a className="link link-hover" target="_blank" rel="noopener noreferrer" href="https://tailwindcss.com/docs/installation">
+            TailWind CSS
+          </a>
+          <a className="link link-hover" target="_blank" rel="noopener noreferrer" href="https://daisyui.com/">
+            DaisyUI
+          </a>
+          <a className="link link-hover" target="_blank" rel="noopener noreferrer" href="https://www.npmjs.com/package/json-server">
+            json-server
+          </a>
+        </nav>
+        <nav>
+          <header className="footer-title">Social</header>
+          <div className="flex ">
+            <Link className="link link-hover" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/bertopolo">
+              <span className="text-4xl">
+                <BiLogoLinkedinSquare />
+              </span>
+            </Link>
+            <Link className="link link-hover mx-2" target="_blank" rel="noopener noreferrer" href="https://github.com/BertoPolo">
+              <span className="text-4xl">{<AiFillGithub />}</span>
+            </Link>
+          </div>
+        </nav>
+      </footer>
+    </div>
   )
 }
 
